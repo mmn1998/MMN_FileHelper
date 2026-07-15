11bdebfa5126bfb039bf59f05e64bf8315d2206a
mmn1998 <nmanymhmdmhdy@gmail.com>
2026-07-15 11:26:02 +0330

    add reflection extension

---
+++ MMN_FileHelper/Extensions/ReflectionExtension.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+namespace MMN_FileHelper.Extensions;
+
+public static class ReflectionExtension
+{
+    public static (int Count, List<string> Names) GetPropertyInfo<T>()
+    {
+        var properties = typeof(T)
+            .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+            .Where(p => !p.Name.Contains("Id")) // case-sensitive exclusion
+            .Select(p => p.Name)
+            .ToList();
+
+        return (properties.Count, properties);
+    }
+}
